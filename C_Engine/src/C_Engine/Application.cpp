367a45748f9bc24c0d82a7a5885279a9ea032372
RicSchultheis <44981009+RicSchultheis@users.noreply.github.com>
2022-10-05 19:55:00 +0200

    Event system first part

--- C_Engine/src/C_Engine/Application.cpp
+++ C_Engine/src/C_Engine/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "C_Engine/Events/ApplicationEvent.h"
+#include "C_Engine/Log.h"
 
 namespace C_Engine {
 
@@ -7,6 +9,16 @@ namespace C_Engine {
 	Application::~Application(){}
 
 	void Application::Run() {
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			CE_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			CE_TRACE(e);
+		}
+
 		while (true);
 	}
 }
--- C_Engine/src/C_Engine/Core.h
+++ C_Engine/src/C_Engine/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error C_Engine only supports Windows!
 #endif
+
+#define BIT(x) (1 << x)
